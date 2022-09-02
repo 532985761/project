330cf8781498197123a560f98e53e683e3f0eb3e
532985761 <532985761@qq.com>
2022-09-02 10:07:41 +0800

    init0

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,10 +1,27 @@
 import http from "@/axios";
 
 /**
- * 测试接口
- *
- * @returns
+ * 获取所有商品
  */
 export function getAllGoods() {
   return http.get("/goods/getAllGoods");
+}
+
+/**
+ * 添加商品
+ */
+export function addGoods(goods:any) {
+  return http.post("/goods/addGoods",goods);
+}
+/**
+ * 编辑商品
+ */
+export function editGoods(goods:any) {
+  return http.post("/goods/editGoods",goods);
+}
+/**
+ * 删除商品
+ */
+export function deleteGoods(id) {
+  return http.post("/goods/deleteGoods/"+id);
 }
